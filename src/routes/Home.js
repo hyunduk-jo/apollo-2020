19654b186fa573e0965be938b0ec19ba1a2062b2
hyunduk-jo <johd321@kakao.com>
2020-11-06 16:22:38 +0900

    getting data & show

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { gql } from "apollo-boost";
 import { useQuery } from '@apollo/react-hooks';
+import Movie from '../components/Movie';
+import styled from 'styled-components';
 
 const GET_MOVIES = gql`
   {
@@ -11,20 +13,70 @@ const GET_MOVIES = gql`
   }
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export const Home = () => {
-  const { loading, error, data } = useQuery(GET_MOVIES);
+  const { loading, data } = useQuery(GET_MOVIES);
 
   return (
     <>
+      <Container>
+        <Header>
+          <Title>Apollo 2020</Title>
+          <Subtitle>I love GraphQL</Subtitle>
+        </Header>
+        {loading && <Loading>Loading...</Loading>}
+        {!loading && data.movies && (
+          <Movies>
+            {data.movies.map(m => (
+              <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+            ))}
+          </Movies>
+        )}
+      </Container>
 
-      {
-        loading === true ?
-          <h1>Loading...</h1> :
-          data.movies.map((m, i) => {
-            return <img src={m.medium_cover_image} alt={m.id} key={i} />
-
-          })
-      }
     </>
   )
 };
